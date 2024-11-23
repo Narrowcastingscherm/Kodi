adb3ec3610f3485a4b3977df4c2fb2a8dbbda00b
Narrowcastingscherm <67043140+Narrowcastingscherm@users.noreply.github.com>
2024-11-23 09:33:52 +0100

    Add files via upload

---
+++ repository/plugin.program.chef20/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://kodi.narrowcastingscherm.nl/build.xml'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://kodi.narrowcastingscherm.nl/nieuws.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
